6bde6785e640265586f54b34b13fe8677a8e90a5
Shaun Shanil Menezes <shaunshanilmenezes@Shauns-MacBook-Air.local>
2023-08-23 11:37:46 +0800

    proper spinners done

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -100,10 +100,13 @@
 				{/if}
 			</div>
 		
-			<div class=" relative login-control w-full mt-4 group">
+			<!-- <div class=" relative login-control w-full mt-4 group">
 					<div class="absolute w-20 h-10 inset-0 bg-gradient-to-r from-pink-600 to-purple-600 opacity-75 rounded-lg blur-xl group-hover:opacity-100 transition duration-200"></div>
 				  <button class=" relative w-20 h-10 bg-black text-gray-300 group-hover:text-white transition duration-200 rounded-lg ">Login</button>
-			</div>
+			</div> -->
+			<div class=" relative login-control w-full mt-4 group">
+				<button class="neon-button">Login</button>
+		  </div>
 		</form>
 
 		
@@ -124,6 +127,88 @@
 
 
 </div>
+
+<style>
+
+:root {
+    --clr-neon: hsl(317 100% 54%);
+    --clr-bg: hsl(323 21% 16%);
+  }
+
+	.neon-button {
+    font-size: 1rem;
+    color: var(--clr-neon);
+    display: inline-block;
+    cursor: pointer;
+    text-decoration: none;
+    border: var(--clr-neon) 0.125em solid;
+    padding: 0.25em 1em;
+    border-radius: 0.25em;
+
+    text-shadow: 
+    0 0 0.125em hsl(0 0% 100% / 0.3),
+    0 0 0.45em var(--clr-neon);
+
+    box-shadow: inset 0 0 0.5em 0 var(--clr-neon), 0 0 0.5em 0 var(--clr-neon);
+    position: relative;
+    transition: background-color 100ms linear;
+
+  }
+
+  .neon-button::after {
+    content: "";
+    position: absolute;
+    top:0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    box-shadow: 0 0 2em 0.5em var(--clr-neon);
+    opacity: 0;
+    transition: opacity 100ms linear;
+    background-color: var(--clr-neon);
+    z-index: -1;
+
+  }
+
+  .neon-button:hover::before,
+  .neon-button:focus::before {
+    opacity: 1;
+  }
+
+  .neon-button:hover::after,
+  .neon-button:focus::after {
+    opacity: 1;
+  }
+
+  .neon-button::before {
+    pointer-events: none;
+    content: "";
+    position: absolute;
+    background: var(--clr-neon);
+    top: 120%;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    transform: perspective(1em) rotateX(40deg) scale(1, 0.35);
+    filter: blur(1.5em);
+    opacity: 0.7;
+  }
+
+  .neon-button:hover,
+  .neon-button:focus {
+    background-color: var(--clr-neon);
+    color: var(--clr-bg);
+    text-shadow: none;
+  }
+
+  .neon-button::after {
+
+  }
+
+
+
+</style>
 
 
 
--- src/spinner/spinner.svelte
+++ src/spinner/spinner.svelte
@@ -6,14 +6,53 @@
 
 
 {#if $loading}
-<div class=" fixed bg-yellow-700 rounded-3xl h-6 w-6">
+<!-- <div class=" fixed bg-yellow-700 rounded-3xl h-6 w-6">
 
-</div>
+</div> -->
+<div class="loader"></div>
 
 {/if}
 
 <style>
-    div {
+
+    .loader {
+        position: fixed;
+        top:0;
+        left:0;
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: black;
+        transition: opacity 0.75s, visibility 0.75s;
+    }
+
+    .loader::after {
+        content: "";
+        width: 75px;
+        height: 75px;
+        border: 15px solid lightgrey;
+        border-top-color: darkviolet ;
+        border-radius: 50%;
+        animation: loading 0.75s ease infinite;
+    }
+
+    @keyframes loading {
+        from{
+            transform: rotate(0turn);
+        } 
+        to{
+            transform: rotate(1turn);
+        }
+    }
+
+
+
+
+
+
+    /* div {
         inset: calc(50% - calc(24px/2));
         animation: moveLoader 1s infinite alternate;
     }
@@ -25,5 +64,6 @@
         to {
             transform: translate3d(-100px, 0, 0);
         }
-    }
+    } */
+
 </style>
